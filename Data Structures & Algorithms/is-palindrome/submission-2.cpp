fdcbc15303b3330d8eac7c39209481953bb8728f
Yashvi <yashvi@tamu.edu>
2026-05-13 22:09:12 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,20 @@
+//O(n) runtime and O(1) space
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        //2 indexes - (for loop?)
+        int i1 = 0;
+        int i2 = s.length()-1;
+        while(i1<i2){
+            while(i1<i2 && !isalnum(s[i1])) i1++;
+            while(i1<i2 && !isalnum(s[i2])) i2--;
+            if(tolower(s[i1]) != tolower(s[i2])){
+                return false;
+            }
+            i1++;
+            i2--;
+        }
+        return true;
+    }
+    
+};
