02d80800eba539bec985b28417f46ee9feed9512
Yashvi <yashvi@tamu.edu>
2026-05-18 17:19:43 -0500

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,17 @@
+//O(1) space and O(n) time
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size()-1;
+        for(int a=0;a<=(s.size()-1)/2;a++){
+            if(i==j)
+                break;
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
